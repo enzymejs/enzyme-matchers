fe8cf7782f2f4a9e140dd4f72311c980d2b38b36
blainekasten <blainekasten@gmail.com>
2016-12-02 19:49:05 -0600

    toHaveTagName update

--- packages/enzyme-matchers/src/assertions/toHaveTagName.js
+++ packages/enzyme-matchers/src/assertions/toHaveTagName.js
@@ -10,21 +10,34 @@ import negateMessage from '../negateMessage';
 import type { Matcher } from '../../../../types/Matcher';
 import type { EnzymeObject } from '../../../../types/EnzymeObject';
 
+import name from '../utils/name';
+import html from '../utils/html';
+
 export default function toHaveTagName(enzymeWrapper:EnzymeObject, tag:string) : Matcher {
+  const wrapperHtml = html(enzymeWrapper);
   if (enzymeWrapper.nodes.length > 1) {
     return {
       pass: false,
       message: `Cannot verify tag name on a wrapper of multiple nodes. Found ${enzymeWrapper.length} nodes.`, // eslint-disable-line max-len
+      negateMessage: ``,
+      contextualInformation: {
+        actual: wrapperHtml,
+        blah: true
+      },
     };
   }
 
-  const pass = enzymeWrapper.type() === tag;
+  const actualTag = enzymeWrapper.type();
+  const pass = actualTag === tag;
+
+  const wrapperName = `<${name(enzymeWrapper)}>`
 
   return {
     pass,
-    message: negateMessage(
-      pass,
-      `Expected node to be of type "${tag}".`
-    ),
+    message: `Expected ${wrapperName} node to equal (using ===) type "${tag}" but it is a "${actualTag}".`,
+    negateMessage: `Expected ${wrapperName} node not to equal (using ===) type "${tag}" but it is that type.`,
+    contextualInformation: {
+      actual: wrapperHtml,
+    },
   };
 }
