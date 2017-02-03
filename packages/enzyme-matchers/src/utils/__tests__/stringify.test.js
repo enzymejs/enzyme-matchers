c38afe880655c1874cb112a63edd8d1c4c643fb4
David Petersen <david@petersendidit.com>
2017-02-03 16:36:11 -0600

    Fix stringify to not crash on null
    stringify would try to `.toString()` on null because `typeof null ===
    'object'`:

--- packages/enzyme-matchers/src/utils/__tests__/stringify.test.js
+++ packages/enzyme-matchers/src/utils/__tests__/stringify.test.js
@@ -6,6 +6,7 @@ describe('stringify', () => {
     proof.falsy = false;
     proof.truthy = true;
     proof.string = 'string';
+    proof.null = null;
     proof.circular = proof;
     proof.class = Promise;
     proof.arry = Object.keys(proof).map(k => proof[k]); // includes all other proofs
