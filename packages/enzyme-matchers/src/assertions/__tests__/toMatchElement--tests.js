d00360b5073ec28e042dfb61fa840968af7c8ed8
Patrick Finnigan <patrick.finnigan@hbo.com>
2017-05-23 09:47:21 -0700

    add toMatchElement API

---
+++ packages/enzyme-matchers/src/assertions/__tests__/toMatchElement--tests.js
@@ -0,0 +1,49 @@
+const { shallow, mount } = require('enzyme');
+const React = require('react');
+
+const toMatchElement = require('../toMatchElement');
+
+function Fixture() {
+  return (
+    <div>
+      <span id="child" className="foo" />
+    </div>
+  );
+}
+
+describe('toMatchElement', () => {
+  [shallow, mount].forEach(builder => {
+    describe(builder.name, () => {
+      const wrapper = builder(<Fixture />);
+      const truthyResults = toMatchElement(wrapper, <Fixture />);
+      const truthyResults2 = toMatchElement(wrapper.find('span'),
+        <span id="child" className="foo" />
+      );
+      const falsyResults = toMatchElement(wrapper, <div />);
+
+      it('returns the pass flag properly', () => {
+        expect(truthyResults.pass).toBeTruthy();
+        expect(truthyResults2.pass).toBeTruthy();
+        expect(falsyResults.pass).toBeFalsy();
+      });
+
+      it('returns the message with the proper pass verbage', () => {
+        expect(truthyResults.message).toMatchSnapshot();
+        expect(truthyResults2.message).toMatchSnapshot();
+        expect(falsyResults.message).toMatchSnapshot();
+      });
+
+      it('returns the message with the proper fail verbage', () => {
+        expect(truthyResults.negatedMessage).toMatchSnapshot();
+        expect(truthyResults2.negatedMessage).toMatchSnapshot();
+        expect(falsyResults.negatedMessage).toMatchSnapshot();
+      });
+
+      it('provides contextual information for the message', () => {
+        expect(truthyResults.contextualInformation).toMatchSnapshot();
+        expect(truthyResults2.contextualInformation).toMatchSnapshot();
+        expect(falsyResults.contextualInformation).toMatchSnapshot();
+      });
+    });
+  });
+});
---
+++ packages/enzyme-matchers/src/assertions/toMatchElement.js
@@ -0,0 +1,35 @@
+/**
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree. *
+ *
+ * @providesModule toMatchElementAssertion
+ * @flow
+ */
+
+import { shallow, mount } from 'enzyme';
+import type { Matcher } from '../../../../types/Matcher';
+import type { EnzymeObject } from '../../../../types/EnzymeObject';
+import isShallowWrapper from '../utils/isShallowWrapper';
+import single from '../utils/single';
+
+function toMatchElement(actualEnzymeWrapper:EnzymeObject, reactInstance:Object) : Matcher {
+  let expectedWrapper:EnzymeObject;
+  if (!isShallowWrapper(actualEnzymeWrapper)) {
+    expectedWrapper = mount(reactInstance);
+  } else {
+    expectedWrapper = shallow(reactInstance);
+  }
+
+  const actual = actualEnzymeWrapper.debug();
+  const expected = expectedWrapper.debug();
+  const pass = actual === expected;
+
+  return {
+    pass,
+    message: 'Expected actual value to match the expected value.',
+    negatedMessage: 'Did not expect actual value to match the expected value.',
+    contextualInformation: `\nactual:\n${actual}\n\nexpected:\n${expected}\n`,
+  };
+}
+
+export default single(toMatchElement);
