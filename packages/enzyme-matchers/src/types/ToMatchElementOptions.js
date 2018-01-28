b7f364fa165ceb5c87080466521b6b2d92defe18
Pat Finnigan <pfinnigan@convoy.com>
2018-01-28 10:00:38 -0800

    ignore props by default for toMatchElement()

---
+++ packages/enzyme-matchers/src/types/ToMatchElementOptions.js
@@ -0,0 +1,11 @@
+/**
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree. *
+ *
+ * @providesModule ToMatchElementOptions
+ * @flow
+ */
+
+export type ToMatchElementOptions = {
+  ignoreProps?: boolean,
+};
--- packages/enzyme-matchers/src/types/index.js
+++ packages/enzyme-matchers/src/types/index.js
@@ -3,3 +3,4 @@
 export type { EnzymeObject } from './EnzymeObject';
 export type { Matcher } from './Matcher';
 export type { MatcherMethods } from './MatcherMethods';
+export type { ToMatchElementOptions } from './ToMatchElementOptions';
