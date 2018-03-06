3e04b1c732ff45e2668be212161ed662ce915233
Simen Bekkhus <sbekkhus91@gmail.com>
2018-03-07 00:05:01 +0100

    remove unnecessary raf polyfill

--- packages/jest-environment-enzyme/src/setup.js
+++ packages/jest-environment-enzyme/src/setup.js
@@ -1,39 +1,4 @@
-/* globals window */
-/* eslint-disable global-require, no-plusplus */
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-// MIT license
-export const polyfillRaf = () => {
-  if (typeof window === 'undefined') return;
-
-  let lastTime = 0;
-  const vendors = ['ms', 'moz', 'webkit', 'o'];
-  for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[`${vendors[x]}RequestAnimationFrame`];
-    window.cancelAnimationFrame =
-      window[`${vendors[x]}CancelAnimationFrame`] ||
-      window[`${vendors[x]}CancelRequestAnimationFrame`];
-  }
-
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function requestAnimationFrame(callback) {
-      const currTime = new Date().getTime();
-      const timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      const id = window.setTimeout(() => {
-        callback(currTime + timeToCall);
-      }, timeToCall);
-      lastTime = currTime + timeToCall;
-      return id;
-    };
-  }
-
-  if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function cancelAnimationFrame(id) {
-      clearTimeout(id);
-    };
-  }
-};
+/* eslint-disable global-require */
 
 export const exposeGlobals = () => {
   let Adapter;
