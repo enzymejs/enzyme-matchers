41e3cd6087205627f910a1f3c20328ff4acd0ca1
Blaine Kasten <blainekasten@gmail.com>
2018-03-07 11:40:06 -0600

    Better linting file support

--- packages/eslint-config-jest-enzyme/index.js
+++ packages/eslint-config-jest-enzyme/index.js
@@ -1,7 +1,20 @@
 module.exports = {
   overrides: [
     {
-      files: ['**/*.test.js', '**/*.test.jsx'],
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/*.test.ts',
+        '**/*.test.tsx',
+        '**/__tests__/**/*.test.js',
+        '**/__tests__/**/*.test.jsx',
+        '**/__tests__/**/*.test.ts',
+        '**/__tests__/**/*.test.tsx',
+        '**/__tests__/**/*.js',
+        '**/__tests__/**/*.jsx',
+        '**/__tests__/**/*.ts',
+        '**/__tests__/**/*.tsx',
+      ],
       globals: {
         React: true,
         mount: true,
